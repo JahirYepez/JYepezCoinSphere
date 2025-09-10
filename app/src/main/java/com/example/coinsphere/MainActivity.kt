4a0b4ba3ba46bb47724e28d188b7cb7af6555dbb
Jahir xd <yepezjahir@gmail.com>
2025-09-10 12:20:51 -0600

    Background, titulo, lazyolumn de cards de informacion

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -4,13 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.coinsphere.models.CardsItem
+import com.example.coinsphere.models.cardsList
+import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +35,47 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoinSphereTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    //
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen(){
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Background)
+            .padding(20.dp)
+    ) {
+        // Titulo
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                //posible padding
+                .weight(1f)
+        ) {
+            Text(
+                text = "CoinSphere",
+                color = Color.White,
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        // Cards
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                //.weight(1f)
+        ) {
+            items(cardsList){ cards ->
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {
+
                 }
             }
         }
@@ -38,10 +90,10 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     CoinSphereTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
